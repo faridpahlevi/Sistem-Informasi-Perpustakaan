832bf2fcf0870818f86ae9c3e1c2143159378e96
liyanurafidah02 <121424250+liyanurafidah02@users.noreply.github.com>
2022-12-26 23:29:58 +0700

    Create v_header.php

---
+++ application/views/admin/v_header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!-- css bootstrap -->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.css' ?>">
+    <!-- css datatables -->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/DataTables/datatables.css' ?>">
+    <!-- icon font awesome -->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/awesome/css/font-awesome.css' ?>">
+    <!-- jquery dan bootstrap js -->
+        <script type="text/javascript" src="<?php echo base_url().'assets/js/jquery.js'?>"></script>
+        <script type="text/javascript" src="<?php echo base_url().'assets/js/bootstrap.js' ?>"></script>
+    <!-- js datatables -->
+        <script type="text/javascript" src="<?php echo base_url().'assets/DataTables/datatables.js' ?>"></script>
+</head>
