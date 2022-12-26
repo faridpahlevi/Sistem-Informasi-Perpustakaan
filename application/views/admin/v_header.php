0f85d38cf78392697189d954a31a9b85eefc71c0
liyanurafidah02 <121424250+liyanurafidah02@users.noreply.github.com>
2022-12-26 23:42:37 +0700

    Update v_header.php

--- application/views/admin/v_header.php
+++ application/views/admin/v_header.php
@@ -17,3 +17,42 @@
     <!-- js datatables -->
         <script type="text/javascript" src="<?php echo base_url().'assets/DataTables/datatables.js' ?>"></script>
 </head>
+	<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?php echo base_url().'admin'; ?>">Perpustakaan</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" arialabel="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="<?php echo base_url().'admin'; ?>"><i class="fa fa-home"></i> Dashboard</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="<?php echo base_url().'admin/petugas'; ?>"><i class="fa fa-user"></i> Petugas</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="<?php echo base_url().'admin/anggota'; ?>"><i class="fa fa-users"></i> Anggota</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="<?php echo base_url().'admin/buku'; ?>"><i class="fa fa-book"></i> Buku</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="<?php echo base_url().'admin/peminjaman_laporan'; ?>"><i class="fa fa-book"></i> Laporan Peminjaman</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="<?php echo base_url().'admin/ganti_password'; ?>"><i class="fa fa-lock"></i> Ganti Password</a>
+                    </li>
+                </ul>
+
+                <span class="navbar-text mr-3 text-center text-light">Halo, <?php echo $this->session->userdata('username'); ?> [admin]</span>
+
+                <a  class="btn btn-outline-light ml-1 nav-link" href="<?php echo base_url().'admin/logout'; ?>"><i class="fa fa-power-off"></i> KELUAR</a>
+            </div>
